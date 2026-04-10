66414380536d04996189face86ec4d0509d92714
Arafat Hossain <arafat240605@gmail.com>
2026-04-11 01:18:41 +0600

    Update Lab_6.cpp

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> // for formatting
 using namespace std;
 
 int main()
@@ -18,6 +19,22 @@ int main()
         for(int i=0;i<n-j;i++)
             diff[i][j] = diff[i+1][j-1] - diff[i][j-1];
 
+    // 🔷 Print Table
+    cout << "\nForward Difference Table:\n\n";
+
+    cout << "x\t y\t del(y)\t del^2(y)\t del^3(y)\t del^4(y)\t del^5(y)\n";
+
+    for(int i=0;i<n;i++)
+    {
+        cout << x[i] << "\t";
+
+        for(int j=0;j<n-i;j++)
+        {
+            cout << diff[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
     float value = 1946;
     float h = x[1] - x[0];
     float p = (value - x[0]) / h;
@@ -33,7 +50,7 @@ int main()
         result = result + (term * diff[0][i]) / fact;
     }
 
-    cout << "Population in 1946 = " << result;
+    cout << "\nPopulation in 1946 = " << result;
 
     return 0;
 }
